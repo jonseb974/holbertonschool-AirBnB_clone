64fd163a416466cd2ef15a2af3b42b68e832a551
Maykha27 <5144@holbertonschool.com>
2022-10-14 16:45:08 +0200

    README.md

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,7 +35,7 @@ class FileStorage:
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
 
-    def reload(self):
+    def (self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
